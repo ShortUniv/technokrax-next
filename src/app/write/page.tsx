1d7039d3af3b2297db48dbaf44eeefcfcfb1a70f
Abdul Almas <abdulalmas696@gmail.com>
2024-06-11 23:20:52 +0530

    first commit

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,7 +1,6 @@
-'use client'
+"use client";
 import { TiptapCollabProvider } from "@hocuspocus/provider";
 import {
-  Suspense,
   useCallback,
   useEffect,
   useLayoutEffect,
@@ -61,8 +60,8 @@ export default function Document() {
   // const searchParams = useSearchParams()
 
   // const hasCollab = parseInt(searchParams.get('noCollab') as string) !== 1
-  const hasCollab = true
-  
+  const hasCollab = true;
+
   const getNanoId = (): string => {
     const nanoid = customAlphabet("6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz", 10);
 
@@ -94,10 +93,10 @@ export default function Document() {
 
   useLayoutEffect(() => {
     if (hasCollab && collabToken) {
-    //   const {
-    //     VITE_PUBLIC_COLLAB_DOC_PREFIX,
-    //     VITE_PUBLIC_TIPTAP_COLLAB_APP_ID,
-    //   } = import.meta.env;
+      //   const {
+      //     VITE_PUBLIC_COLLAB_DOC_PREFIX,
+      //     VITE_PUBLIC_TIPTAP_COLLAB_APP_ID,
+      //   } = import.meta.env;
 
       setProvider(
         new TiptapCollabProvider({
@@ -126,131 +125,9 @@ export default function Document() {
 
   return (
     <>
-    <Suspense fallback={<div>Loading...</div>}>
-
       {DarkModeSwitcher}
       <BlockEditor hasCollab={hasCollab} ydoc={ydoc} provider={provider} />
-    </Suspense>
     </>
   );
 }
 
-// 'use client';
-
-// import { TiptapCollabProvider } from "@hocuspocus/provider";
-// import {
-//   useCallback,
-//   useEffect,
-//   useLayoutEffect,
-//   useMemo,
-//   useState,
-// } from "react";
-// import * as Y from "yjs";
-// import { BlockEditor } from "@/components/WriteArticle/components/BlockEditor";
-// import { createPortal } from "react-dom";
-// import { Surface } from "@/components/WriteArticle/components/ui/Surface";
-// import { Toolbar } from "@/components/WriteArticle/components/ui/Toolbar";
-// import { Icon } from "@/components/WriteArticle/components/ui/Icon";
-// import { customAlphabet } from "nanoid";
-// import { useDispatch } from "react-redux";
-// import { getToken } from "@/actions/HomePage";
-// import { useSearchParams } from "next/navigation";
-
-// const useDarkmode = () => {
-//   const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
-
-//   useEffect(() => {
-//     const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
-//     const handleChange = () => setIsDarkMode(mediaQuery.matches);
-//     mediaQuery.addEventListener("change", handleChange);
-//     return () => mediaQuery.removeEventListener("change", handleChange);
-//   }, []);
-
-//   useEffect(() => {
-//     document.documentElement.classList.toggle("dark", isDarkMode);
-//   }, [isDarkMode]);
-
-//   const toggleDarkMode = useCallback(
-//     () => setIsDarkMode((isDark) => !isDark),
-//     []
-//   );
-//   const lightMode = useCallback(() => setIsDarkMode(false), []);
-//   const darkMode = useCallback(() => setIsDarkMode(true), []);
-
-//   return {
-//     isDarkMode,
-//     toggleDarkMode,
-//     lightMode,
-//     darkMode,
-//   };
-// };
-
-// export default function Document() {
-//   const { isDarkMode, darkMode, lightMode } = useDarkmode();
-//   const [provider, setProvider] = useState<TiptapCollabProvider | null>(null);
-//   const [collabToken, setCollabToken] = useState<string | null>(null);
-//   const [room, setRoom] = useState("");
-//   const [hasCollab, setHasCollab] = useState(false);
-//   const dispatch = useDispatch();
-//   const searchParams = useSearchParams();
-
-//   useEffect(() => {
-//     const fetchCollabInfo = async () => {
-//       try {
-//         const collabParam = searchParams.get('noCollab');
-//         setHasCollab(parseInt(collabParam as string) !== 1);
-
-//         dispatch<any>(getToken());
-
-//         const token =
-//           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3MTE0NTYxMTN9.TWMhnHRqcB1BmER4zXF-eyebbeslIKs2JPvB-PiL6zA";
-
-//         setCollabToken(token);
-        
-//         const nanoid = customAlphabet("6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz", 10);
-//         const roomId = nanoid();
-//         setRoom(roomId);
-//       } catch (error) {
-//         console.error("Error fetching collaboration data:", error);
-//       }
-//     };
-
-//     fetchCollabInfo();
-//   }, [dispatch, searchParams]);
-
-//   const ydoc = useMemo(() => new Y.Doc(), []);
-
-//   useLayoutEffect(() => {
-//     if (hasCollab && collabToken) {
-//       const newProvider = new TiptapCollabProvider({
-//         name: `doc_${room}`,
-//         appId: "8mz0zl9x" ?? "",
-//         token: collabToken,
-//         document: ydoc,
-//       });
-
-//       setProvider(newProvider);
-//     }
-//   }, [collabToken, ydoc, room, hasCollab]);
-
-//   if (hasCollab && (!collabToken || !provider)) return null; // Render nothing until the provider is ready
-
-//   const DarkModeSwitcher = createPortal(
-//     <Surface className="flex items-center gap-1 fixed bottom-6 right-6 z-[99999] p-1">
-//       <Toolbar.Button onClick={lightMode} active={!isDarkMode}>
-//         <Icon name="Sun" />
-//       </Toolbar.Button>
-//       <Toolbar.Button onClick={darkMode} active={isDarkMode}>
-//         <Icon name="Moon" />
-//       </Toolbar.Button>
-//     </Surface>,
-//     document.body
-//   );
-
-//   return (
-//     <>
-//       {DarkModeSwitcher}
-//       <BlockEditor hasCollab={hasCollab} ydoc={ydoc} provider={provider} />
-//     </>
-//   );
-// }
